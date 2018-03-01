ea431d28a6dfa63677fbf77e4c3a6887c1e9d073
Wagner Costa <wcaquino@gmail.com>
2018-03-01 08:49:17 -0300

    EditText

--- src/main/java/br/ce/wcaquino/appium/FormularioTeste.java
+++ src/main/java/br/ce/wcaquino/appium/FormularioTeste.java
@@ -2,10 +2,15 @@ package br.ce.wcaquino.appium;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
@@ -13,16 +18,30 @@ import io.appium.java_client.remote.MobileCapabilityType;
 public class FormularioTeste {
 
 	@Test
-	public void deveInstalarAPK() throws MalformedURLException {
+	public void devePreecherCampoTexto() throws MalformedURLException {
 		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
 	    desiredCapabilities.setCapability("platformName", "Android");
 	    desiredCapabilities.setCapability("deviceName", "emulator-5554");
 	    desiredCapabilities.setCapability("automationName", "uiautomator2");
-//	    desiredCapabilities.setCapability("appPackage", "com.android.calculator2");
-//	    desiredCapabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
 	    desiredCapabilities.setCapability(MobileCapabilityType.APP, "/Users/wcaquino/Documents/dev/java/workspaceCurso/CursoAppium/src/main/resources/CTAppium_1_0.apk");
 	    
 	    AndroidDriver<MobileElement> driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	    
+	    //Selecionar formulario
+	    List<MobileElement> elementosEncontrados = driver.findElements(By.className("android.widget.TextView"));
+//	    for(MobileElement elemento: elementosEncontrados) {
+//	    	System.out.println(elemento.getText());
+//	    }
+	    elementosEncontrados.get(1).click();
+	    
+	    //escrever nome
+	    MobileElement campoNome = driver.findElement(MobileBy.AccessibilityId("nome"));
+	    campoNome.sendKeys("Wagner");
+	    
+	    //checar nome escrito
+	    String text = campoNome.getText();
+	    Assert.assertEquals("Wagner", text);
 	    
 	    driver.quit();
 	}
