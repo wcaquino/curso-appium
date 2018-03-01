f1c4dad35f925a769e18e450606f4ad8477e72c8
Wagner Costa <wcaquino@gmail.com>
2018-03-01 13:18:55 -0300

    reset app

--- src/main/java/br/ce/wcaquino/appium/core/BaseTest.java
+++ src/main/java/br/ce/wcaquino/appium/core/BaseTest.java
@@ -1,11 +1,17 @@
 package br.ce.wcaquino.appium.core;
 
 import org.junit.After;
+import org.junit.AfterClass;
 
 public class BaseTest {
 
+	@AfterClass
+	public static void finalizaClasse(){
+		DriverFactory.killDriver();
+	}
+	
     @After
     public void tearDown() {
-        DriverFactory.killDriver();
+    	DriverFactory.getDriver().resetApp();
     }
 }
